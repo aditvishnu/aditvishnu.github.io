c085ff7d3b16735dd54749e010056680a478176d
aditvishnu <127286828+aditvishnu@users.noreply.github.com>
2025-08-29 00:38:58 +0000

    Deploying to gh-pages from @ aditvishnu/aditvishnu.github.io@87162fe7455d15e836ab8eecbb5d2abc48f1cf29 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -76,6 +76,13 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=WwcPb7MAAAAJ", "_blank");
         },
       },{
+        id: 'social-semanticscholar',
+        title: 'Semantic Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.semanticscholar.org/author/2374052383", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
