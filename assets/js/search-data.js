bfb2b4ce26a717ff3fea289d999477906db4a5b8
aditvishnu <127286828+aditvishnu@users.noreply.github.com>
2025-09-26 04:44:16 +0000

    Deploying to gh-pages from @ aditvishnu/aditvishnu.github.io@ff88c7b150d265c26ee763adefba174b040cd74b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -34,7 +34,7 @@ ninja.data = [{
       
         title: 'Hello World! <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
       
-      description: "Welcome to my blog. This is my first post. Excited to see where this goes!",
+      description: "Welcome to my blog. This is my first post. Excited to see where this goes! Meanwhile, take a look at About.",
       section: "Posts",
       handler: () => {
         
@@ -51,41 +51,6 @@ ninja.data = [{
           title: 'Invited to speak at the International Workshop on Quantum Boltzmann Machines this December....',
           description: "",
           section: "News",},{
-        id: 'social-bluesky',
-        title: 'Bluesky',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://bsky.app/profile/aditvishnu.bsky.social", "_blank");
-        },
-      },{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%61%64%69%74%76%69%73%68%6E%75@%69%69%73%63.%61%63.%69%6E", "_blank");
-        },
-      },{
-        id: 'social-linkedin',
-        title: 'LinkedIn',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.linkedin.com/in/adit-vishnu-57487514b", "_blank");
-        },
-      },{
-        id: 'social-wordpress_url',
-        title: 'Wordpress_url',
-        section: 'Socials',
-        handler: () => {
-          window.open("", "_blank");
-        },
-      },{
-        id: 'social-x',
-        title: 'X',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://twitter.com/adit_vishnu", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
